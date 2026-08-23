4508e6da58f869200c9886c4d6d103096cf49b7a
borudeyash1 <143342463+borudeyash1@users.noreply.github.com>
2026-08-23 21:19:29 +0530

    IDS-IPS DevOps: publish build to ids-ips-build-2026-08-23

---
+++ server/src/types/slugify.d.ts
@@ -0,0 +1,14 @@
+declare module 'slugify' {
+    interface SlugifyOptions {
+        replacement?: string;
+        remove?: RegExp;
+        lower?: boolean;
+        strict?: boolean;
+        locale?: string;
+        trim?: boolean;
+    }
+
+    function slugify(string: string, options?: SlugifyOptions | string): string;
+
+    export default slugify;
+}

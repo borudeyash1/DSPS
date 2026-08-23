1a8381a6b02f6a1a356f42621edf6dfec4747030
borudeyash1 <143342463+borudeyash1@users.noreply.github.com>
2026-08-23 21:10:57 +0530

    IDS-IPS DevOps: publish build to ids-ips-build-2026-08-23

---
+++ client/src/pages/admin/MenPageBuilderPage.tsx
@@ -0,0 +1,7 @@
+import PageBuilder from '../../components/admin/PageBuilder';
+
+const MenPageBuilderPage = () => {
+    return <PageBuilder pageType="men" pageTitle="Men's Page Builder" />;
+};
+
+export default MenPageBuilderPage;

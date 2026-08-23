ccd7e050e9f83d870a87447aa87b20ae4c38f6ee
borudeyash1 <143342463+borudeyash1@users.noreply.github.com>
2026-08-23 21:10:05 +0530

    IDS-IPS DevOps: publish build to ids-ips-build-2026-08-23

---
+++ client/src/components/Toast.tsx
@@ -0,0 +1,63 @@
+import { useEffect } from 'react';
+import './Toast.css';
+
+export type ToastType = 'success' | 'error' | 'warning' | 'info';
+
+interface ToastProps {
+  message: string;
+  type: ToastType;
+  onClose: () => void;
+  duration?: number;
+}
+
+const Toast = ({ message, type, onClose, duration = 3000 }: ToastProps) => {
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      onClose();
+    }, duration);
+
+    return () => clearTimeout(timer);
+  }, [duration, onClose]);
+
+  const getIcon = () => {
+    switch (type) {
+      case 'success':
+        return (
+          <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+            <path d="M0 0h24v24H0z" fill="none"></path>
+            <path d="M15.795 8.342l-5.909 9.545a1 1 0 0 1-1.628 0l-3.182-4.909a1 1 0 0 1 1.629-1.165l2.556 3.953L14.165 7.51a1 1 0 0 1 1.63 1.165z"></path>
+          </svg>
+        );
+      case 'error':
+        return (
+          <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+            <path d="M0 0h24v24H0z" fill="none"></path>
+            <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm1 15h-2v-2h2v2zm0-4h-2V7h2v6z"></path>
+          </svg>
+        );
+      case 'warning':
+        return (
+          <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+            <path d="M0 0h24v24H0z" fill="none"></path>
+            <path d="M1 21h22L12 2 1 21zm12-3h-2v-2h2v2zm0-4h-2v-4h2v4z"></path>
+          </svg>
+        );
+      case 'info':
+        return (
+          <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+            <path d="M0 0h24v24H0z" fill="none"></path>
+            <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm1 15h-2v-6h2v6zm0-8h-2V7h2v2z"></path>
+          </svg>
+        );
+    }
+  };
+
+  return (
+    <div className={`toast toast-${type}`}>
+      <div className="toast-content">{message}</div>
+      <div className="toast-icon">{getIcon()}</div>
+    </div>
+  );
+};
+
+export default Toast;

34e37e0bbb71b988311cfb537a6f029328f6dd6c
borudeyash1 <143342463+borudeyash1@users.noreply.github.com>
2026-08-23 21:10:55 +0530

    IDS-IPS DevOps: publish build to ids-ips-build-2026-08-23

---
+++ client/src/pages/admin/KidsPageBuilderPage.tsx
@@ -0,0 +1,7 @@
+import PageBuilder from '../../components/admin/PageBuilder';
+
+const KidsPageBuilderPage = () => {
+    return <PageBuilder pageType="kids" pageTitle="Kids' Page Builder" />;
+};
+
+export default KidsPageBuilderPage;

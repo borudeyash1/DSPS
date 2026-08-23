a3aea6feb0904aa82fd2a0ac3aefc2017290004f
borudeyash1 <143342463+borudeyash1@users.noreply.github.com>
2026-08-23 21:16:28 +0530

    IDS-IPS DevOps: publish build to ids-ips-build-2026-08-23

---
+++ server/seedData.js
@@ -0,0 +1,14 @@
+module.exports = [
+  {
+    name: 'Product 1',
+    description: 'Description of Product 1',
+    price: 100,
+    image: 'server/uploads/images/product/product_1769512988751_IMG_8431.PNG'
+  },
+  {
+    name: 'Product 2',
+    description: 'Description of Product 2',
+    price: 200,
+    image: 'server/uploads/images/product/product_1769513011808_IMG_8431.PNG'
+  }
+];
